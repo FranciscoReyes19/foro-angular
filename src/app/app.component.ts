4c5b7a2a7f829ce760716c9244881e7b8b493862
FranciscoReyes19 <francisco.reyesfrt@outlook.com>
2020-02-27 03:30:43 -0600

    cambios2702

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
+import { UserService } from './services/user.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[UserService]
 })
-export class AppComponent {
-  title = 'foro-angular';
+export class AppComponent implements OnInit, DoCheck {
+  public title = 'Foro-Angular';
+  public identity;
+  public token;
+
+  constructor(
+  	private _userService: UserService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  	){
+  		this.identity = this._userService.getIdentity();
+  		this.token = this._userService.getToken();
+  }
+
+  ngOnInit(){
+  	console.log(this.identity);
+  	console.log(this.token);
+  }
+
+  //Para refrescar barra automaticamente
+  ngDoCheck(){
+    this.identity = this._userService.getIdentity();
+  }
+
+  logout(){
+      localStorage.clear();
+      //localStorage.setItem('token', null);
+      this.identity = null;
+      this.token = null;
+      this._router.navigate(['/inicio']);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { routing, appRoutingProviders } from './app.routing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+  appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
